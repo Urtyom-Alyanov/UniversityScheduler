8b987284aac73f77ce9958cc4964f511adc62cf4
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-04-24 00:18:04 +0300

    init: unit tests

---
+++ UniversityScheduler.Models.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityScheduler.Models.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ UniversityScheduler.Services.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityScheduler.Services.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
