2682b72296d6efe10f9a469a29f09462a0da595e
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-04-24 23:38:48 +0300

    chore: delete unused usings and new namespace

--- source/UniversityScheduler.UI/App.xaml.cs
+++ source/UniversityScheduler.UI/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace UniversityScheduler;
+namespace UniversityScheduler.UI;
 
 /// <summary>
 /// Interaction logic for App.xaml
